92f7d71e7fbf8d18dbc60b428e878b6e3619f32b
Nagendra Kathi <nagendra30244@gmail.com>
2025-08-14 10:00:40 +0530

    Complete Backend setup

--- backend/validators/jobValidation.js
+++ backend/validators/jobValidation.js
@@ -4,7 +4,7 @@ const createJobSchema = Joi.object({
   title: Joi.string().min(3).max(100).required(),
   company: Joi.string().min(2).max(100).required(),
   location: Joi.string().min(2).max(100).required(),
-  jobType: Joi.string().valid('FullTime', 'Parttime', 'Contract', 'Internship').required(),
+  jobType: Joi.string().valid('FullTime', 'Parttime', 'Contract', 'Internship').insensitive().required(),
   salaryRange: Joi.string().pattern(/^\d+-\d+$/).required(),
   description: Joi.string().min(10).required(),
   requirements: Joi.string().allow(''),
