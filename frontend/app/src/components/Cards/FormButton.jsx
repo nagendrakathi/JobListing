cb564e7412c7f146865578b67467218671d5bed3
Nagendra Kathi <nagendra30244@gmail.com>
2025-08-15 14:41:52 +0530

    Style Button in form

--- frontend/app/src/components/Cards/FormButton.jsx
+++ frontend/app/src/components/Cards/FormButton.jsx
@@ -10,9 +10,9 @@ export default function FormButton({
   const baseClasses =
     "flex justify-center items-center gap-2 px-6 py-3 rounded-lg font-semibold text-[16px] transition shadow cursor-pointer w-1/3";
   const variantClasses = {
-    primary: "bg-[#00AAFF] text-white hover:bg-[#27b7ff] border-2 border-[#00AAFF]",
+    primary: "bg-[#00AAFF] text-white hover:bg-[#27b7ff] border-1 border-[#00AAFF]",
     secondary:
-      "border-2 border-[#000000] bg-white text-[#232323] hover:bg-[#f8f8f8]",
+      "border-1 border-[#000000] bg-white text-[#232323] hover:bg-[#f8f8f8]",
   };
 
   return (
