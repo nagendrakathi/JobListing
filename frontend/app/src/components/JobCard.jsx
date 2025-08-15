84770bc47782f7acc0c94f33a59154438f9b165b
Nagendra Kathi <nagendra30244@gmail.com>
2025-08-15 13:49:27 +0530

    Modify JobCard Styles

--- frontend/app/src/components/JobCard.jsx
+++ frontend/app/src/components/JobCard.jsx
@@ -68,7 +68,7 @@ export default function JobCard({
       <div className="text-[20px] font-bold text-[#202124] mb-[10px] mt-2">
         {title}
       </div>
-      <div className="flex items-center text-[#535353] text-[16px] font-medium mb-3 gap-2">
+      <div className="flex items-center justify-between text-[#535353] text-[16px] font-medium mb-3 gap-2">
         {experience && (
           <span className="w-full flex items-center gap-1">
             <img src="/jobCardIcons/exp.svg" alt=""/>
